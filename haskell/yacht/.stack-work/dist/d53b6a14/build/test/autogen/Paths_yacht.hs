a579334f9d9406b89864691f4a840bb3c0b11bc5
tobz619 <93312805+tobz619@users.noreply.github.com>
2023-01-15 21:37:10 +0000

    yacht scoring complete

---
+++ haskell/yacht/.stack-work/dist/d53b6a14/build/test/autogen/Paths_yacht.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+{-# OPTIONS_GHC -Wno-missing-safe-haskell-mode #-}
+module Paths_yacht (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [1,2,0,0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "C:\\Users\\Tobi\\Exercism\\haskell\\yacht\\.stack-work\\install\\b18ebbf0\\bin"
+libdir     = "C:\\Users\\Tobi\\Exercism\\haskell\\yacht\\.stack-work\\install\\b18ebbf0\\lib\\x86_64-windows-ghc-9.0.2\\yacht-1.2.0.0-F1UsM2sWKWB1AzgMqfa2q8-test"
+dynlibdir  = "C:\\Users\\Tobi\\Exercism\\haskell\\yacht\\.stack-work\\install\\b18ebbf0\\lib\\x86_64-windows-ghc-9.0.2"
+datadir    = "C:\\Users\\Tobi\\Exercism\\haskell\\yacht\\.stack-work\\install\\b18ebbf0\\share\\x86_64-windows-ghc-9.0.2\\yacht-1.2.0.0"
+libexecdir = "C:\\Users\\Tobi\\Exercism\\haskell\\yacht\\.stack-work\\install\\b18ebbf0\\libexec\\x86_64-windows-ghc-9.0.2\\yacht-1.2.0.0"
+sysconfdir = "C:\\Users\\Tobi\\Exercism\\haskell\\yacht\\.stack-work\\install\\b18ebbf0\\etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "yacht_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "yacht_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "yacht_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "yacht_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "yacht_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "yacht_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "\\" ++ name)
--- haskell/yacht/src/Yacht.hs
+++ haskell/yacht/src/Yacht.hs
@@ -1,5 +1,7 @@
 module Yacht (yacht, Category(..)) where
 
+import qualified Data.Map as Map
+
 data Category = Ones
               | Twos
               | Threes
@@ -11,7 +13,41 @@ data Category = Ones
               | LittleStraight
               | BigStraight
               | Choice
-              | Yacht
+              | Yacht deriving Eq
 
 yacht :: Category -> [Int] -> Int
-yacht category dice = error "You need to implement this function."
+yacht Ones xs = foldr (\x -> if x == 1 then (+1) else (+0)) 0 xs
+yacht Twos xs = foldr (\x -> if x == 2 then (+2) else (+0)) 0 xs
+yacht Threes xs = foldr (\x -> if x == 3 then (+3) else (+0)) 0 xs
+yacht Fours xs = foldr (\x -> if x == 4 then (+4) else (+0)) 0 xs
+yacht Fives xs = foldr (\x -> if x == 5 then (+5) else (+0)) 0 xs
+yacht Sixes xs = foldr (\x -> if x == 6 then (+6) else (+0)) 0 xs
+
+yacht FullHouse xs = check $ count (take 5 xs)
+                     where check [(a,3),(b,2)] = 3 * a + b * 2
+                           check [(a,2),(b,3)] = a * 2 + b * 3
+                           check _ = 0
+
+yacht FourOfAKind xs = four $ count (take 5 xs)
+                     where four = foldr (\(a,c) acc -> if c >= 4 then 4*a else 0) 0 . filter (\(_,c) -> c >= 4)
+
+yacht LittleStraight xs
+       | count xs == zip [1..5] [1,1..] = 30 
+       | otherwise = 0 
+
+yacht BigStraight xs
+       | count xs == zip [2..6] [1,1..] = 30
+       | otherwise = 0
+
+yacht Choice xs = sum xs
+
+yacht Yacht xs = check $ count xs
+              where check [(_,5)] = 50
+                    check _ = 0
+
+
+count :: Ord a => [a] -> [(a,Int)]
+count xs = Map.toList $ foldr occurs Map.empty xs
+            where occurs x acc
+                   | Map.lookup x acc == Nothing = Map.insert x 1 acc
+                   | otherwise = Map.alter (fmap (+1)) x acc 
