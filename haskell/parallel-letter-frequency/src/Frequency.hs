e0d962a8e8fce4f4277307a39662597ea86461b7
tobz619 <93312805+tobz619@users.noreply.github.com>
2023-03-02 18:38:43 +0000

    Frequency Iter1 added

--- haskell/parallel-letter-frequency/src/Frequency.hs
+++ haskell/parallel-letter-frequency/src/Frequency.hs
@@ -1,7 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BangPatterns #-}
+
 module Frequency (frequency) where
 
-import Data.Map  (Map)
-import Data.Text (Text)
+import qualified Data.Map as Map
+import qualified Data.Text as T
+import Data.Char (isAlpha, toLower)
+import Control.Concurrent
+import Control.Parallel
+import Control.Monad.State
 
-frequency :: Int -> [Text] -> Map Char Int
-frequency nWorkers texts = error "You need to implement this function."
+frequency :: Int -> [T.Text] -> Map.Map Char Int
+frequency nWorkers texts = letters (removeSpace texts) Map.empty
+
+removeSpace :: [T.Text] -> String
+removeSpace = map toLower . filter isAlpha . concatMap T.unpack
+
+
+-- | Counts the number of occurences of the first letter 
+--   and returns a new string omitting that letter
+--   and a map containing a count of that letter
+--
+-- >>> runState (letterFreq "Apple") Map.empty
+-- ("pple",fromList [('A',1)])
+--
+letterFreq :: String -> State (Map.Map Char Int) String 
+letterFreq xs = state $ \mp -> isLet xs mp (safeHead xs)
+            where safeHead (x:_) = Just x
+                  safeHead  _    = Nothing
+
+                  isLet str acca (Just x)  =  let !singl = Map.insert x (length . filter (== toLower x) 
+                                                            . map toLower $ str) acca
+
+                                                  !filtered = filter (/= x) str
+                                               in filtered `par` (singl `pseq`
+                                                  (filtered,singl))
+                  
+                  isLet _ acca Nothing =  ([], acca)
+
+letters ::  String -> Map.Map Char Int -> Map.Map Char Int
+letters xs acc  = case remainder of
+              [] -> mp
+              _ -> letters remainder mp
+            where (!remainder, !mp) =  runState (letterFreq xs) acc            
