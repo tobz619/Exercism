02e332bfb4f7becb3464e80965d1b20b1cbd6647
tobz619 <93312805+tobz619@users.noreply.github.com>
2023-03-21 13:49:41 +0000

    final edits to Dominos

--- haskell/dominoes/src/Dominoes.hs
+++ haskell/dominoes/src/Dominoes.hs
@@ -32,16 +32,3 @@ solver = go []
       where go acc [] = pure acc
             go acc ds = do (added, newAcc) <- eligibleChainBuilder acc ds
                            go newAcc (delete added ds)
-
-twelve :: [(Int, Int)]
-twelve = [(1, 2), (5, 3), (3, 1), (1, 2), (2, 4), (1, 6), (2, 3), (3, 4), (5, 6), (3,6), (4,5), (2,1)]
-
-three :: [(Int, Int)]
-three = [(1, 2), (3, 1), (2, 3)]
-
-{- 1. Take every domino and run it with validNexts to get all valid neighbours.
-   2. If it validNexts returns Nothing, leave that domino in the potential chain list;
-      if Just xs then remove the first occurrence of that domino from the list.
-   3. Run non-deterministically.
-   4. Ensure all 
--}
