9199dbb664956d52a183c04047de2753c3af564f
tobz619 <93312805+tobz619@users.noreply.github.com>
2023-05-03 16:10:07 +0100

    robot name completed

--- haskell/robot-name/src/Robot.hs
+++ haskell/robot-name/src/Robot.hs
@@ -2,19 +2,22 @@ module Robot (Robot, initialState, mkRobot, resetName, robotName) where
 
 import Control.Monad.State
 import System.Random
+import Control.Concurrent.MVar
 import Data.List
 
-newtype Robot = Robot String deriving (Show, Eq)
-type RunState = [Robot]
+type Robot = MVar String
+type RunState = [String]
 
 initialState :: RunState
 initialState = []
 
 mkRobot :: StateT RunState IO Robot
-mkRobot = StateT f where f acc =  do name <- liftIO genName
-                                     if Robot name `notElem` acc
-                                        then return (Robot name, Robot name:acc)
-                                        else runStateT mkRobot acc
+mkRobot = StateT f where f acc = do name <- genName
+                                    if name `elem` acc
+                                       then runStateT mkRobot acc
+                                       else do newName <- liftIO $ newMVar name
+                                               return (newName, name:acc)
+                                  
 
 
 genName :: IO String
@@ -22,10 +25,15 @@ genName = (++) <$> liftIO (replicateM 2 (getStdRandom (randomR ('A','Z'))))
                <*> liftIO (replicateM 3 (getStdRandom (randomR ('0','9'))))
 
 resetName :: Robot -> StateT RunState IO ()
-resetName robot = do robots <- get
-                     newName <- liftIO $ evalStateT mkRobot robots
-                     modify (robot `delete`)
-                     modify (newName :)
+resetName robot = do robo <- liftIO $ readMVar robot
+                     newName <- liftIO genName
+                     acc <- get
+                     if newName `elem` acc
+                       then resetName robot
+                       else do inita <- liftIO $ swapMVar robot newName
+                               modify (delete robo)
+                               modify (inita:)
+
 
 robotName :: Robot -> IO String
-robotName (Robot s) = pure s
+robotName = readMVar      
