d077959e8e6ca623e425c68b7c31073979b1c3c9
tobz619 <93312805+tobz619@users.noreply.github.com>
2023-05-16 17:09:59 +0100

    spiralMaker working

--- haskell/spiral-matrix/src/Spiral.hs
+++ haskell/spiral-matrix/src/Spiral.hs
@@ -5,12 +5,6 @@ data Spiral a = Turn a (Spiral a) | Forward a (Spiral a) | End deriving Show
 spiral :: Int -> [[Int]]
 spiral size = error "You need to implement this function."
 
-makeSquares :: (Num a, Enum a) => a -> [a]
-makeSquares n = [1..n^2]
-
-spiralMaker :: Int -> Spiral Int
-spiralMaker n = fromList n 1 [1..n^2]
-
 fromList :: Int -> Int -> [a] -> Spiral a
 fromList _ _ [] = End
 fromList n t xs = go n t (n-1) xs
@@ -18,3 +12,12 @@ fromList n t xs = go n t (n-1) xs
               go n 0 n' xs = fromList (n-1) 2 xs
               go n t 0 (x:xs)  = Turn x (go n (t-1) (n-1) xs)
               go n t n' (x:xs) = Forward x (go n t (n' - 1) xs)
+
+spiralMaker :: Int -> Spiral Int
+spiralMaker n = fromList n 1 [1..n^2]
+
+spiralToLists :: Spiral a -> [[a]]
+spiralToLists = go []
+           where go acc End = pure acc
+                 go acc (Turn a b) = (a : acc) : spiralToLists b
+                 go acc (Forward a b) = go (a:acc) b
