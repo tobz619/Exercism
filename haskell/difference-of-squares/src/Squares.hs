2d568d71c69d303551d27ad5c1cedc37470872dc
tobz619 <93312805+tobz619@users.noreply.github.com>
2023-07-08 15:02:31 +0100

    other wider missed merges

--- haskell/difference-of-squares/src/Squares.hs
+++ haskell/difference-of-squares/src/Squares.hs
@@ -7,4 +7,4 @@ squareOfSum :: Integral a => a -> a
 squareOfSum n = sum [1..n]  ^ 2
 
 sumOfSquares :: Integral a => a -> a
-sumOfSquares n = sum [i^2 | i <- [1 .. n]]
+sumOfSquares n = sum . map (^2) $ [1..n]
