530d649df35191cb77342b922d626a933f9f0568
tobz619 <93312805+tobz619@users.noreply.github.com>
2023-08-08 22:08:23 +0100

    Stinky IORef implementation needed to pass the tests here, why not State???

--- haskell/linked-list/src/Deque.hs
+++ haskell/linked-list/src/Deque.hs
@@ -1,18 +1,58 @@
 module Deque (Deque, mkDeque, pop, push, shift, unshift) where
 
-data Deque a = Dummy
+import Data.IORef
 
-mkDeque :: IO (Deque a)
-mkDeque = error "You need to implement this function."
+data Deque a = End | Node { prev :: Deque a, current :: a, next :: Deque a} 
+    deriving (Show, Eq)
 
-pop :: Deque a -> IO (Maybe a)
-pop deque = error "You need to implement this function."
+mkDeque :: IO (IORef (Deque a))
+mkDeque = newIORef End
 
-push :: Deque a -> a -> IO ()
-push deque x = error "You need to implement this function."
+pop :: IORef (Deque a) -> IO (Maybe a)
+pop ref = do queue <- readIORef ref
+             case queue of
+                End -> pure Nothing
+                nodes -> getLast nodes
+                    where getLast (Node _ x End) = do modifyIORef ref removeLast
+                                                      return $ Just x
+                          getLast n = getLast (next n)
+                          removeLast node@(Node p x End) = Node (prev p) (current p) End
+                          removeLast n = removeLast (next n)
 
-unshift :: Deque a -> a -> IO ()
+
+push :: IORef (Deque a) -> a -> IO ()
+push ref x = do queue <- readIORef ref 
+                case queue of
+                 End -> writeIORef ref (Node End x End)
+                 nodes -> modifyIORef ref modLast
+                    where modLast n@(Node p c End) = let newNode = Node n x End
+                                                      in Node p c newNode
+                          modLast n = modLast (next n)
+                          
+                    
+
+unshift :: IORef (Deque a) -> a -> IO ()
 unshift deque x = error "You need to implement this function."
 
-shift :: Deque a -> IO (Maybe a)
+shift :: IORef (Deque a) -> IO (Maybe a)
 shift deque = error "You need to implement this function."
+
+test = do
+      deque <- mkDeque
+      push deque 'a'
+      push deque 'b'
+      pop deque >>= print
+      pop deque >>= print
+
+test2 = do
+      deque <- mkDeque
+      push deque 'a'
+      push deque 'b'
+      pop deque >>= print
+      push deque 'c'
+      shift deque >>= print
+      unshift deque 'd'
+      push deque 'e'
+      shift deque >>= print
+      pop deque >>= print
+      pop deque >>= print
