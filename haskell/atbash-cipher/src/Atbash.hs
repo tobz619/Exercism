b37f0e3c1865a75062f1c6770df44ba2ade969ec
tobz619 <93312805+tobz619@users.noreply.github.com>
2023-11-20 19:57:43 +0000

    atbash completed

--- haskell/atbash-cipher/src/Atbash.hs
+++ haskell/atbash-cipher/src/Atbash.hs
@@ -1,7 +1,28 @@
+{-#LANGUAGE OverloadedStrings #-}
 module Atbash (decode, encode) where
 
-decode :: String -> String
-decode cipherText = error "You need to implement this function."
+import qualified Data.Text as T
+import           Data.Text (Text)
+import Data.Char (isUpper, toLower, isSpace, isAlphaNum)
 
-encode :: String -> String
-encode plainText = error "You need to implement this function."
+decode :: Text -> Text
+decode = T.filter isAlphaNum . encode
+
+encode :: Text -> Text
+encode = T.unwords . T.chunksOf 5 . T.foldr encodeChar "" 
+
+alphabet :: Text
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+backwards :: Text
+backwards = T.reverse alphabet
+
+pairs :: [(Char, Char)]
+pairs = T.zip alphabet backwards
+
+encodeChar :: Char -> Text -> Text
+encodeChar a acc
+    | not . isAlphaNum $ a = acc
+    | isUpper a = maybe (a `T.cons` acc) (`T.cons` acc) (lookup (toLower a) pairs)
+    | otherwise = maybe (a `T.cons` acc) (`T.cons` acc) (lookup a pairs)
+
