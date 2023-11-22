5524bad8102774300f55e2743c0864ca4c2d0918
tobz619 <93312805+tobz619@users.noreply.github.com>
2023-11-22 19:49:00 +0000

    complex numbers done

--- haskell/complex-numbers/src/ComplexNumbers.hs
+++ haskell/complex-numbers/src/ComplexNumbers.hs
@@ -12,38 +12,40 @@ module ComplexNumbers
  complex) where
 
 import Prelude hiding (div, abs, exp)
-
+import qualified Prelude as P (exp)
 -- Data definition -------------------------------------------------------------
-data Complex a = Dummy deriving(Eq, Show)
+data Complex a = Complex {r :: a, i :: a} deriving (Show, Eq)
 
-complex :: (a, a) -> Complex a
-complex = error "You need to implement this function"
+complex :: Num a => (a, a) -> Complex a
+complex (a,b) = Complex a b
 
 -- unary operators -------------------------------------------------------------
 conjugate :: Num a => Complex a -> Complex a
-conjugate = error "You need to implement this function"
+conjugate c = curry complex (r c) (negate (i c))
 
 abs :: Floating a => Complex a -> a
-abs = error "You need to implement this function"
+abs c = sqrt (r c ^ 2 + i c ^ 2)
 
 real :: Num a => Complex a -> a
-real = error "You need to implement this function"
+real = r
 
 imaginary :: Num a => Complex a -> a
-imaginary = error "You need to implement this function"
+imaginary = i
 
 exp :: Floating a => Complex a -> Complex a
-exp = error "You need to implement this function"
-
+exp c =  let dist = P.exp (r c)
+          in Complex (dist * cos (i c)) (dist * sin (i c))
 -- binary operators ------------------------------------------------------------
 mul :: Num a => Complex a -> Complex a -> Complex a
-mul = error "You need to implement this function"
+mul (Complex a b) (Complex c d) = curry complex (a*c - b*d) (b*c+a*d)
 
 add :: Num a => Complex a -> Complex a -> Complex a
-add = error "You need to implement this function"
+add (Complex a b) (Complex c d) = curry complex (a+c) (b+d)
 
 sub :: Num a => Complex a -> Complex a -> Complex a
-sub = error "You need to implement this function"
+sub (Complex a b) (Complex c d) = curry complex (a-c) (b-d)
 
 div :: Fractional a => Complex a -> Complex a -> Complex a
-div = error "You need to implement this function"
+div (Complex a b) (Complex c d) = curry complex 
+                                         ( (a*c + b*d)/(c^2 + d^2) ) 
+                                         ( (b*c - a*d)/(c^2 + d^2) )
