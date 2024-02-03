becf8d24060b64008dbe5a4a7d95f3413af49557
Tobi Oloke <tobzdaman619@gmail.com>
2024-02-03 15:44:15 +0000

    Go counting completed with plagiarised answer: will read over to see how it truly works one day

--- haskell/go-counting/src/Counting.hs
+++ haskell/go-counting/src/Counting.hs
@@ -4,26 +4,36 @@ module Counting (
     territoryFor
 ) where
 
+{-
+Stole
+-}
+
 import Data.Set (Set)
-import qualified Data.Set as Set
+import qualified Data.Set as S
 import qualified Data.Map as Map
-import Data.Maybe (mapMaybe, catMaybes)
-import Control.Monad.State
-import Data.List (unfoldr)
+import Data.Maybe (mapMaybe)
+import Data.List (find)
+import Debug.Trace (traceShowId)
+import Data.Tuple (swap)
 
 data Color = Black | White | Empty deriving (Eq, Ord, Show)
 type Coord = (Int, Int)
 data Stone = Stone {coord :: Coord, colour :: Color} deriving (Eq, Ord, Show)
-type Board a = Map.Map Coord a
+type Board = Map.Map Coord Color
+
+newtype Graph a = Graph (Map.Map a [a]) deriving (Show, Eq, Ord)
 
 territories :: [String] -> [(Set Coord, Maybe Color)]
-territories board = error "You need to implement this function."
+territories board = map 
+                    (\coordSet -> (S.map swap coordSet, getColour madeBoard coordSet)) 
+                    (expandGroups madeBoard)
+                where madeBoard = mkBoard board
 
 territoryFor :: [String] -> Coord -> Maybe (Set Coord, Maybe Color)
-territoryFor board coord = error "You need to implement this function."
+territoryFor board co = find (S.member co . fst) (territories  board)
 
-mkBoard :: [String] -> Board Color
-mkBoard xs = Map.fromList $ go (1,1) (unlines xs) []
+mkBoard :: [String] -> Board
+mkBoard xs = Map.fromList $! go (1,1) (unlines xs) []
         where go _ [] _ = []
               go (r,_) ('\n':rest) acc = go (r+1, 1) rest acc
               go (r,c) ('B':ys) acc = ((r,c), Black) : go (r,c+1) ys acc
@@ -35,52 +45,59 @@ board5x5 =  [ "  B  "
             , "B W B"
             , " W W "
             , "  W  " ]
--- >>> example
--- fromList [((1,1),Empty),((1,2),Empty),((1,3),Black),((1,4),Empty),((1,5),Empty),((2,1),Empty),((2,2),Black),((2,3),Empty),((2,4),Black),((2,5),Empty),((3,1),Black),((3,2),Empty),((3,3),White),((3,4),Empty),((3,5),Black),((4,1),Empty),((4,2),White),((4,3),Empty),((4,4),White),((4,5),Empty),((5,1),Empty),((5,2),Empty),((5,3),White),((5,4),Empty),((5,5),Empty)]
-example :: Board Color
+
+example :: Board
 example = mkBoard board5x5
 
---- >>> stoneNeighbours (Stone (3,3) Black) example
--- [Stone {coord = (3,2), color = Empty},Stone {coord = (2,3), color = Empty},Stone {coord = (3,4), color = Empty},Stone {coord = (4,3), color = Empty}]
-stoneNeighbours :: Stone -> Board Color -> [Stone]
-stoneNeighbours (Stone (r,c) _) b = mapMaybe checkColour [lf, up, ri, dw]
-            where lf = (r,c-1)
-                  up = (r-1,c)
-                  ri = (r,c+1)
-                  dw = (r+1,c)
-                  checkColour co = Stone co <$> Map.lookup co b
 
--- >>> sameColourNeighbours (Stone (3,3) Black) example
--- []
-sameColourNeighbours :: Stone -> Board Color -> [Stone]
-sameColourNeighbours s b = filter ((== colour s) . colour) (stoneNeighbours s b)
+example2 :: Board
+example2 = mkBoard ["BB ", "BBB", "WWW"]
 
-enemyColour :: Color -> Maybe Color
-enemyColour Black = Just White
-enemyColour White = Just Black
-enemyColour _ = Nothing
+eligibleNeighbours :: Coord -> Board -> [Coord]
+eligibleNeighbours c b = mapMaybe checkColour (neighbours c)
+                  where checkColour co = co <$ Map.lookup co b
 
-isConnectedToEnemy :: Board Color -> Stone -> Bool
-isConnectedToEnemy b (Stone (r,c) col) = enemyColour col `elem` map (`Map.lookup` b) [lf, up, ri, dw]
-                                    where lf = (r,c-1)
-                                          up = (r-1,c)
-                                          ri = (r,c+1)
-                                          dw = (r+1,c)
+neighbours :: Coord -> [Coord]
+neighbours (r,c) = [(r+1,c),(r-1,c),(r,c+1),(r,c-1)]
 
 
+getColour :: Board -> Set Coord -> Maybe Color
+getColour b grp
+    | S.null adj = Nothing
+    | Just Black `S.member` adj 
+      && Just White `S.member` adj = Nothing
+    | Just Black `S.member` adj =  Just Black
+    | otherwise = Just White
+    where
+        adj = let allNeighbours =  S.fromList 
+                                    (concatMap (`eligibleNeighbours` b) grp) 
+                                    `S.difference` grp 
 
--- ProgressCancelledException
-stepSearch :: Board Color -- The current Board
-           -> [Coord]  -- Coord queue
-           -> [Stone] -- Stones of interest
-           -> Maybe ([Stone], [Coord]) -- Tuple of seen coords and new queue
-stepSearch _ _ [] = Nothing
-stepSearch b seen (x:xs) = Just (xs ++ new, coord x: seen)
-                where new =
-                        filter (and .sequence [(`notElem` seen) . coord, (`notElem` xs)])
-                            $ stoneNeighbours x b <> concatMap (`stoneNeighbours` b) xs
+               in S.map (`Map.lookup` b) allNeighbours
+               
+-- >>> expandGroups example2
+-- [fromList [(1,3)]]
+expandGroups :: Board -> [Set Coord]
+expandGroups board = go empties
+        where empties = Map.filter (== Empty) board
+              go pool
+               | Map.null pool = []
+               | otherwise = let emptyPoint = fst $ Map.findMin pool
+                                 (grp, newPool) =
+                                    go' (Map.delete emptyPoint pool)
+                                        (S.singleton emptyPoint)
+                                        S.empty
+                              in grp : go newPool
 
+              go' pool stack found
+                | S.null stack = (found, pool)
+                | otherwise = let
+                    adjacents = S.fromList $ concatMap (`eligibleNeighbours` board) stack
+                    poolSet = S.fromList $ Map.keys pool
+                    newStack = S.intersection poolSet adjacents
+                    newPool = Map.filterWithKey checkKeys board
+                                where checkKeys k _ = S.member k poolSet'
+                                      poolSet' = S.difference poolSet newStack 
+                    newFound = S.union found stack
+                    in go' newPool newStack newFound
 
-
-bfs :: Board Color -> Stone -> [Coord] -> [[Stone]]
-bfs b initialStone = unfoldr (\q -> stepSearch b q [initialStone]) 
