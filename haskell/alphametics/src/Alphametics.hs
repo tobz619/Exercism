d130ccea1606ef9fceee1583d9533259546e4cec
Tobi Oloke <tobzdaman619@gmail.com>
2024-02-13 22:22:17 +0000

    Our correct answer finally shows up within the mess BUT it's surrounded by noise! Must find a way to filter out leading zeros

--- haskell/alphametics/src/Alphametics.hs
+++ haskell/alphametics/src/Alphametics.hs
@@ -26,10 +26,11 @@ solve puzzle = error "You need to implement this function."
 updateMap ::  PossibleChars -> [(Char, Int)] -> PossibleChars
 updateMap = foldr (uncurry setChar)
         where setChar ch v = Map.mapWithKey (selector ch)
-                where selector _ ' ' _ = [0]
+                where selector ' ' _ xs = xs
+                      selector _ ' ' xs = xs
                       selector toChange key xs
-                            | key == toChange = [v]
-                            | otherwise = delete v xs
+                        | key == toChange = [v]
+                        | otherwise = delete v xs
 
 
 checkNull :: (Int, PossibleChars) -> Maybe (Int, PossibleChars)
@@ -38,7 +39,7 @@ checkNull (c,mp) = (,) c <$> traverse (\x -> if null x then Nothing else Just x)
 
 addChars :: String -> Char -> Int -> Map.Map Char [Int] -> Maybe [(Int, PossibleChars)]
 addChars cs resChar carry charMap = do
-    as <- mapM (`Map.lookup` charMap) cs
+    as <- traverse (`Map.lookup` charMap) cs
     resVals <- resChar `Map.lookup` charMap
     let valCombi = sequence as
 
@@ -60,8 +61,8 @@ processFun f str ch car charMap = let
 loop = go 0
     where go _ _ _ [] mp = pure mp
           go carry f [] _ mp = pure mp
-          go carry f cols@(s:strs) res@(c:chrs) !charMap = let
-            !carryRes = (carry, cols,res, charMap) `traceShow` processFun f s c carry charMap
+          go carry f (s:strs) (c:chrs) !charMap = let
+            !carryRes = processFun f s c carry charMap
             in concatMap (\ ~(newCar, newMap) -> go newCar f strs chrs newMap) carryRes
 
 standardiseInps :: [String] -> String -> ([String], String)
@@ -83,11 +84,11 @@ initChars xs = Map.insert ' ' [0] $ foldr initiator Map.empty (nub xs)
 
 
 -- >>> testInp 
--- []
+-- [fromList [(' ',[0]),('D',[1]),('E',[5]),('M',[0]),('N',[3]),('O',[8]),('R',[2]),('S',[7]),('Y',[6])],fromList [(' ',[0]),('D',[1]),('E',[7]),('M',[0]),('N',[3]),('O',[6]),('R',[4]),('S',[5]),('Y',[8])],fromList [(' ',[0]),('D',[1]),('E',[8]),('M',[0]),('N',[2]),('O',[4]),('R',[6]),('S',[3]),('Y',[9])],fromList [(' ',[0]),('D',[1]),('E',[8]),('M',[0]),('N',[5]),('O',[7]),('R',[3]),('S',[6]),('Y',[9])],fromList [(' ',[0]),('D',[2]),('E',[4]),('M',[0]),('N',[3]),('O',[9]),('R',[1]),('S',[8]),('Y',[6])],fromList [(' ',[0]),('D',[2]),('E',[5]),('M',[0]),('N',[4]),('O',[9]),('R',[1]),('S',[8]),('Y',[7])],fromList [(' ',[0]),('D',[2]),('E',[7]),('M',[0]),('N',[1]),('O',[4]),('R',[6]),('S',[3]),('Y',[9])],fromList [(' ',[0]),('D',[2]),('E',[7]),('M',[0]),('N',[3]),('O',[6]),('R',[4]),('S',[5]),('Y',[9])],fromList [(' ',[0]),('D',[3]),('E',[6]),('M',[0]),('N',[4]),('O',[8]),('R',[2]),('S',[7]),('Y',[9])],fromList [(' ',[0]),('D',[3]),('E',[8]),('M',[0]),('N',[5]),('O',[7]),('R',[2]),('S',[6]),('Y',[1])],fromList [(' ',[0]),('D',[4]),('E',[3]),('M',[0]),('N',[2]),('O',[9]),('R',[1]),('S',[8]),('Y',[7])],fromList [(' ',[0]),('D',[4]),('E',[5]),('M',[0]),('N',[2]),('O',[7]),('R',[3]),('S',[6]),('Y',[9])],fromList [(' ',[0]),('D',[4]),('E',[5]),('M',[0]),('N',[3]),('O',[8]),('R',[2]),('S',[7]),('Y',[9])],fromList [(' ',[0]),('D',[5]),('E',[4]),('M',[0]),('N',[1]),('O',[7]),('R',[3]),('S',[6]),('Y',[9])],fromList [(' ',[0]),('D',[6]),('E',[3]),('M',[0]),('N',[1]),('O',[8]),('R',[2]),('S',[7]),('Y',[9])],fromList [(' ',[0]),('D',[7]),('E',[5]),('M',[1]),('N',[6]),('O',[0]),('R',[8]),('S',[9]),('Y',[2])],fromList [(' ',[0]),('D',[7]),('E',[8]),('M',[0]),('N',[1]),('O',[3]),('R',[6]),('S',[2]),('Y',[5])],fromList [(' ',[0]),('D',[9]),('E',[4]),('M',[0]),('N',[1]),('O',[7]),('R',[2]),('S',[6]),('Y',[3])],fromList [(' ',[0]),('D',[9]),('E',[4]),('M',[0]),('N',[2]),('O',[8]),('R',[1]),('S',[7]),('Y',[3])],fromList [(' ',[0]),('D',[9]),('E',[5]),('M',[0]),('N',[3]),('O',[8]),('R',[1]),('S',[7]),('Y',[4])],fromList [(' ',[0]),('D',[9]),('E',[6]),('M',[0]),('N',[4]),('O',[8]),('R',[1]),('S',[7]),('Y',[5])],fromList [(' ',[0]),('D',[9]),('E',[7]),('M',[0]),('N',[1]),('O',[4]),('R',[5]),('S',[3]),('Y',[6])],fromList [(' ',[0]),('D',[9]),('E',[8]),('M',[0]),('N',[1]),('O',[3]),('R',[6]),('S',[2]),('Y',[7])],fromList [(' ',[0]),('D',[9]),('E',[8]),('M',[0]),('N',[2]),('O',[4]),('R',[5]),('S',[3]),('Y',[7])],fromList [(' ',[0]),('D',[9]),('E',[8]),('M',[0]),('N',[4]),('O',[6]),('R',[3]),('S',[5]),('Y',[7])]]
 testInp = loop addChars cols chrs mp
     where mp = initChars "SENDMOREMONEY"
           (cols, chrs) = pairColumns ["SEND","MORE"] "MONEY"
 
 -- >>> testMap
 -- Just (fromList [(' ',[0]),('b',[0,1,2,3,4,6,7,8,9]),('i',[0,1,2,3,4,6,7,8,9]),('o',[0,1,2,3,4,6,7,8,9]),('t',[5])])
-testMap = traverse (\x -> if null x then Nothing else Just x) $ updateMap (initChars "tobi") [('t',5),('t',5)]
+testMap = traverse (\x -> if null x then Nothing else Just x) $ updateMap (initChars "tobi") [(' ',5),('t',5)]
