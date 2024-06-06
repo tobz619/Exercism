80d969878d31601807a3461c1a8e2675315d5023
Tobi Oloke <tobzdaman619@gmail.com>
2024-06-06 08:17:58 +0100

    We're close but no cigar!

--- haskell/go-counting/src/Counting.hs
+++ haskell/go-counting/src/Counting.hs
@@ -10,13 +10,12 @@ import qualified Data.Set as Set
 import Data.Sequence(Seq, ViewL(..))
 import qualified Data.Sequence as Seq
 import qualified Data.Map as Map
-import Data.Foldable (find)
-import Control.Applicative (Applicative(liftA2))
-import Data.Maybe (mapMaybe)
+import Data.Maybe (fromMaybe, mapMaybe)
 import Control.Monad.State
 import Control.Monad.Reader
+import Debug.Trace
 
-data Color = Black | White | Empty deriving (Eq, Ord, Show)
+data Color = Black | White deriving (Eq, Ord, Show)
 type Coord = (Int, Int)
 data Stone = Stone {coord :: Coord, colour :: Color} deriving (Eq, Ord, Show)
 
@@ -28,38 +27,36 @@ data BFS = BFS { queue :: Seq Coord
 newBFS :: BFS
 newBFS = BFS Seq.empty Set.empty Set.empty
 
-type Board = Map.Map Coord Color
-
-newtype Graph a = Graph (Map.Map a [a]) deriving (Show, Eq, Ord)
+type Board = Map.Map Coord (Maybe Color)
 
 territories :: [String] -> [(Set Coord, Maybe Color)]
-territories board = undefined
+territories board = Set.toList $ Set.fromList $ mapMaybe (territoryFor board) (Map.keys . Map.filter (== Nothing) $ mkBoard board)
 
 territoryFor :: [String] -> Coord -> Maybe (Set Coord, Maybe Color)
-territoryFor board co = undefined
-
+territoryFor board co = do _ <- Map.lookup co (mkBoard board) 
+                           return $ evalState (territorySearch (mkBoard board) co) newBFS
 
 neighbourCoords :: Coord -> [Coord]
-neighbourCoords (r,c) = [(x,y) | x <- [r-1 .. r+1], y<- [c-1 .. c+1] , (x,y) /= (r,c)]
+neighbourCoords (r,c) = [(x,y) | x <- [r-1, r+1], y<- [c-1, c+1] , (x,y) /= (r,c)]
 
-getStoneNeighbours :: Map.Map Coord Color -> Stone -> [Coord]
-getStoneNeighbours board st = mapMaybe (`sameColour` board) . neighbourCoords $ coord st
-                where sameColour co b = do nCol <- Map.lookup co b
-                                           if nCol == colour st
-                                            then Just co
-                                            else Nothing
+territoryNeighbourCoords :: Coord -> [Coord]
+territoryNeighbourCoords (r,c) = [(r, c-1), (r, c+1), (r+1, c), (r-1, c)]
 
+filterSameColourNeighbours :: (Coord -> [Coord]) -> Board -> Coord -> Maybe [Coord]
+filterSameColourNeighbours neighfn board co = do col <- Map.lookup co board
+                                                 let neighbours = neighfn co
+                                                 return $ [ coor | coor <- neighbours, Just col == Map.lookup coor board]
 mkBoard :: [String] -> Board
 mkBoard xs = Map.fromList $! go (1,1) (unlines xs) []
         where go _ [] _ = []
               go (r,_) ('\n':rest) acc = go (r+1, 1) rest acc
-              go (r,c) ('B':ys) acc = ((r,c), Black) : go (r,c+1) ys acc
-              go (r,c) ('W':ys) acc = ((r,c), White) : go (r,c+1) ys acc
-              go (r,c) (_:ys) acc = ((r,c), Empty) : go (r,c+1) ys acc
+              go (r,c) ('B':ys) acc = ((r,c), Just Black) : go (r,c+1) ys acc
+              go (r,c) ('W':ys) acc = ((r,c), Just White) : go (r,c+1) ys acc
+              go (r,c) (_:ys) acc = ((r,c), Nothing) : go (r,c+1) ys acc
 
-newBFSearch :: Stone -> State BFS ()
-newBFSearch st = modify (\bfs -> bfs { queue = searchFrom })
-    where searchFrom = Seq.singleton (coord st)
+newBFSearch :: Coord -> State BFS ()
+newBFSearch co = modify (\bfs -> bfs { queue = searchFrom })
+    where searchFrom = Seq.singleton co
 
 popQueue :: State (Seq Coord) (Maybe Coord)
 popQueue = do q <- gets Seq.viewl
@@ -74,36 +71,76 @@ addToSeen co = asks $ Set.insert co
 addToConnected :: Coord -> Reader (Set Coord) (Set Coord)
 addToConnected co = asks $ Set.insert co
 
-extendQueue :: Foldable t => Map.Map Coord Color -> t Coord -> Stone -> Reader (Seq Coord) (Seq Coord)
-extendQueue board seenList st = do
-    let neighbours = Set.fromList $ getStoneNeighbours board st
+extendQueue :: Foldable t => (Coord -> [Coord]) -> Board -> t Coord -> Coord -> Reader (Seq Coord) (Seq Coord)
+extendQueue neighfn board seenList co = do
+    let neighbours = Set.fromList $ fromMaybe [] $ filterSameColourNeighbours neighfn board co
         unseen =  Set.filter (`notElem` seenList) neighbours
-        newQueue que = foldr (flip (Seq.|>)) que unseen
+        newQueue que = Set.foldl' (Seq.|>) que unseen
     asks newQueue
 
-executeBFS :: Map.Map Coord Color -> Stone -> State BFS (Set Coord)
-executeBFS board st = do
+capturedRegion :: Board -> Set Coord -> Maybe Color
+capturedRegion board regionCoords  = let
+    neighbourCols = Set.fromList $ mapMaybe (`Map.lookup` board) (concatMap neighbourCoords regionCoords)
+    in eval neighbourCols
+        where eval s
+                | (Just Black `elem` s) == (Just White `notElem` s) = Just Black
+                | (Just White `elem` s) == (Just Black `notElem` s) = Just White
+                | otherwise = Nothing 
+
+continueBFS :: Board -> State BFS (Set Coord, Maybe Color)
+continueBFS board = do
+    q <- gets queue
+    viewed <- gets seen
+    c <- gets connected
+    let (res, newQueue) = runState popQueue q
+    traceShowM newQueue
+    case res of
+        Nothing -> return (c, capturedRegion board c)
+        Just co -> do let newSeen = runReader (addToSeen co) viewed
+                          moreQueue = runReader (extendQueue neighbourCoords board newSeen co) newQueue
+                          newConnected = runReader (addToConnected co) c
+                      put $ BFS moreQueue newSeen newConnected
+                      continueBFS board
+
+territoryBFS :: Board -> State BFS (Set Coord, Maybe Color)
+territoryBFS board = do
     q <- gets queue
     viewed <- gets seen
     c <- gets connected
     let (res, newQueue) = runState popQueue q
     case res of
-        Nothing -> return c
+        Nothing -> return (c, capturedRegion board c)
         Just co -> do let newSeen = runReader (addToSeen co) viewed
-                          moreQueue = runReader (extendQueue board newSeen st) newQueue
+                          moreQueue = runReader (extendQueue territoryNeighbourCoords board viewed co) newQueue
                           newConnected = runReader (addToConnected co) c
                       put $ BFS moreQueue newSeen newConnected
-                      executeBFS board st
+                      territoryBFS board
 
-wholeBFS :: Map.Map Coord Color -> Stone -> State BFS (Set Coord)
-wholeBFS board st = do newBFSearch st
-                       executeBFS board st
+territorySearch :: Board -> Coord -> State BFS (Set Coord, Maybe Color)
+territorySearch board co = do newBFSearch co
+                              territoryBFS board
 
+connectedSearch :: Board -> Coord -> State BFS (Set Coord, Maybe Color)
+connectedSearch board co = do newBFSearch co
+                              continueBFS board
 
+board5x5 :: [String]
 board5x5 = [ "  B  "
             , " B B "
             , "B W B"
             , " W W "
             , "  W  " ]
 
-test = evalState (wholeBFS (mkBoard board5x5) (Stone (3,4) White)) newBFS
+board5x5B :: Board
+board5x5B = mkBoard  [ "  B  "
+                    , " B B "
+                    , "B W B"
+                    , " W W "
+                    , "  W  " ]
+
+-- | Tests the thing 
+--
+-- >>> test
+--
+test :: Int -> Int -> (Set Coord, Maybe Color)
+test x y = evalState (territorySearch board5x5B (x,y)) newBFS
